6c210e6d3864ea07f22e82f297a442fe9a9c8f6b
Tempron <wickgrenm@gmail.com>
2023-10-14 00:20:23 +0200

    Compilation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Initialize GLFW
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    // Create a GLFW window
+    GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL Triangle", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Poll for and process events
+        glfwPollEvents();
+
+        // Clear the screen with a black background
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw a triangle
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(0.0f, 0.5f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex2f(-0.5f, -0.5f);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
     return 0;
 }
